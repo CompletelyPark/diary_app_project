9bf14eff4e9270200f91f4b644d0c857ff200bca
Jinwan <dhwhd114@gmail.com>
2024-08-12 20:00:37 +0900

    0812

--- app/src/main/java/com/jinwan/appproject/fragment/CalendarFragment.java
+++ app/src/main/java/com/jinwan/appproject/fragment/CalendarFragment.java
@@ -1,7 +1,6 @@
 package com.jinwan.appproject.fragment;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,29 +8,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.EditText;
-
+import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.jinwan.appproject.Data.Schedule;
 import com.jinwan.appproject.R;
 import com.jinwan.appproject.recycler.ScheduleAdapter;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment{
 
     private CalendarView calendarView;
     private RecyclerView recyclerView;
     private Button toggleCalendar;
+    private ImageButton btn_plus;
 
-//  데이터
+    FragmentManager fragmentManager = getParentFragmentManager();
+
+    //  데이터
     private List<Schedule> scheduleList;
 
 //  recyclerview adapter
@@ -46,7 +51,7 @@ public class CalendarFragment extends Fragment {
         calendarView = view.findViewById(R.id.calendarView);
         recyclerView = view.findViewById(R.id.recyclerView);
         toggleCalendar = view.findViewById(R.id.toggleCalendar);
-
+        btn_plus =view.findViewById(R.id.btn_plus);
         toggleCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,8 +66,16 @@ public class CalendarFragment extends Fragment {
                     Log.d("Tag", "close");
                     istf = true;
                     toggleCalendar.setBackgroundDrawable(getResources().getDrawable(R.drawable.arrow_downward_24px));
-
                 }
+            }
+        });
+
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerdialog();
+
+
             }
         });
 
@@ -71,8 +84,6 @@ public class CalendarFragment extends Fragment {
         recyclerView.setAdapter(scheduleAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-
-
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
@@ -80,7 +91,6 @@ public class CalendarFragment extends Fragment {
                 showScheduleDialog(year, month, dayOfMonth);
             }
         });
-
         return  view;
     }
 
@@ -106,12 +116,31 @@ public class CalendarFragment extends Fragment {
         builder.show();
     }
 
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd){
+    private void DatePickerdialog() {
+        // Creating a MaterialDatePicker builder for selecting a date range
+        MaterialDatePicker.Builder<androidx.core.util.Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("Select a date range");
 
-    }
-    public void onTimeSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth, int yearEnd, int monthOfYearEnd, int dayOfMonthEnd){
-        String hourString = hourO
+        // Building the date picker dialog
+        MaterialDatePicker<Pair<Long, Long>> datePicker = builder.build();
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+
+            // Retrieving the selected start and end dates
+            Long startDate = selection.first;
+            Long endDate = selection.second;
+
+            // Formating the selected dates as strings
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            String startDateString = sdf.format(new Date(startDate));
+            String endDateString = sdf.format(new Date(endDate));
+
+            // Creating the date range string
+            String selectedDateRange = startDateString + " - " + endDateString;
+
+        });
+
+        // Showing the date picker dialog
+        datePicker.show(getSupportFragmentManager(), "DATE_PICKER");
     }
 
 }
