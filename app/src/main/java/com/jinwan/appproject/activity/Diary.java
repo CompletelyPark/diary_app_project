ef1a57b25d29d9b9d308c8c30627d4ac57dc1aa4
Jinwan <dhwhd114@gmail.com>
2024-08-18 16:53:35 +0900

    0818

--- app/src/main/java/com/jinwan/appproject/activity/Diary.java
+++ app/src/main/java/com/jinwan/appproject/activity/Diary.java
@@ -1,16 +1,35 @@
 package com.jinwan.appproject.activity;
 
-import android.content.Intent;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.jinwan.appproject.R;
 
 public class Diary extends BaseActivity {
 
     private Button button;
+    private BottomAppBar bottomAppBar;
+    private EditText daily_diary_text;
+
+    private boolean isbold = false;
+    private boolean isitalic = false;
+
+    private int align = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +43,70 @@ public class Diary extends BaseActivity {
                 finish();
             }
         });
+
+        daily_diary_text = findViewById(R.id.daily_diary_text);
+
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String text = daily_diary_text.getText().toString();
+                SpannableString spannableString = new SpannableString(text);
+//              text font
+                if(item.getItemId()==R.id.text_font){
+                    showDialog();
+                    return true;
+                }
+
+//              font size
+                else if(item.getItemId()==R.id.font_size){
+                    return true;
+                }
+
+//              isbold
+                else if (item.getItemId() == R.id.text_bold) {
+                    return true;
+                }
+                else if (item.getItemId() == R.id.text_italic) {
+                    return true;
+                }
+
+//              underline
+                else if(item.getItemId()==R.id.text_underline){
+                    return true;
+                }
+
+//              text align
+                else if(item.getItemId()==R.id.text_align){
+                    Drawable drawable;
+                    if (align == 0) {
+                        drawable = getResources().getDrawable(R.drawable.format_align_center_48px, null);
+                        daily_diary_text.setGravity(Gravity.CENTER_HORIZONTAL);
+                        item.setIcon(drawable);
+                        align = 1;
+                    } else if (align == 1) {
+                        drawable = getResources().getDrawable(R.drawable.format_align_right_48px, null);
+                        daily_diary_text.setGravity(Gravity.RIGHT);
+
+                        item.setIcon(drawable);
+                        align = 2;
+                    } else if (align ==2){
+                        drawable = getResources().getDrawable(R.drawable.format_align_left_48px, null);
+                        daily_diary_text.setGravity(Gravity.LEFT);
+
+                        item.setIcon(drawable);
+                        align = 0;
+                    }
+                    return true;
+                }
+//              저장 버튼
+                else if(item.getItemId()==R.id.btn_check){
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+
 }
